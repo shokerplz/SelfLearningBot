11fa9949852021b0e95cfa1da4a1d9c7b97aa5a8
Ivan Kovalev <admin@Ivans-MacBook-Pro.local>
2019-02-06 20:50:31 +0300

    Disabled voice messages function

--- bot.py
+++ bot.py
@@ -28,11 +28,12 @@ def download(message):
 def voice_message(message):
     print("You are heard")
     print(message.voice.file_id)
-    download(message)
-    voice = audio.start()
-    if (len(voice) > 0):
-        textMessage(voice, message)
-    else:
+    bot.send_message(message.from_user.id, "Функция приема голосовых сообщений временно недоступна")
+    #download(message)
+    #voice = audio.start()
+    #if (len(voice) > 0):
+        #textMessage(voice, message)
+    #else:
         bot.send_message(message.from_user.id, "Я ничего не понял, повторите еще раз")
 
 @bot.message_handler(content_types=['text'])
